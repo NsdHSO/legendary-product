9f3abc2038f25e2b4ca5e69688463d4c4e326f69
NSDHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-12-03 23:04:48 +0200

    feat: 🎸 added base for dao

--- src/invoice/entities/invoice.entity.ts
+++ src/invoice/entities/invoice.entity.ts
@@ -1,1 +1,9 @@
-export class Invoice {}
+export class Invoice {
+  service: string;
+  startDate: string;
+  endDate: string;
+  hours: number;
+  rate: number;
+  //TODO: NEXT I have to create new DAO for customer
+  customer: string;
+}
