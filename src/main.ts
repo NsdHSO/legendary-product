669a51ddb68fb6c32852453c44792ae687f3d93c
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-05-29 13:43:26 +0300

    chore:🌻 added some logi

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 declare const module: any;
 const allowlist = ['http://localhost:4201', 'http://localhost:4200'];
-const corsOptionsDelegate = function(req, callback) {
+const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
-  if(allowlist.indexOf(req.header('Origin')) !== -1) {
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  }
-  else {
+  } else {
     corsOptions = { origin: false }; // disable CORS for this request
   }
   callback(null, corsOptions); // callback expects two parameters: error and options
@@ -17,8 +17,13 @@ const corsOptionsDelegate = function(req, callback) {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(corsOptionsDelegate);
-  await app.listen(3001);
-  if(module.hot) {
+  await app
+    .listen(process.env.PORT, '0.0.0.0')
+    .then((value) => {
+      console.log('App has started in ', process.env.PORT);
+    })
+    .catch((err) => console.log('Error occur', err));
+  if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
